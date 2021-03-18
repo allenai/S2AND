0e910f6407d4f49c0f431bdef8dda3cd6d7c0571
Sergey Feldman <sergeyfeldman@gmail.com>
2021-03-18 11:57:14 -0700

    nits

--- s2and/consts.py
+++ s2and/consts.py
@@ -15,18 +15,18 @@ except NameError:
 CONFIG_LOCATION = os.path.join(PROJECT_ROOT_PATH, "data", "path_config.json")
 with open(CONFIG_LOCATION) as _json_file:
     CONFIG = json.load(_json_file)
-    
+
 if CONFIG["main_data_dir"] == "absolute path of wherever you downloaded the data to":
-    logger.warning("You haven't set the `main_data_dir` in data/path_config.json! Using data/ as the default data directory.")
+    logger.warning("You haven't set `main_data_dir` in data/path_config.json! Using data/ as default data directory.")
     CONFIG["main_data_dir"] = os.path.join(PROJECT_ROOT_PATH, "data")
 
-assert os.path.exists(CONFIG["main_data_dir"]), f"The `main_data_dir` specified in data/path_config.json does not exist."
-    
+assert os.path.exists(CONFIG["main_data_dir"]), "The `main_data_dir` specified in data/path_config.json doesn't exist."
+
 # paths
 NAME_COUNTS_PATH = os.path.join(CONFIG["main_data_dir"], "name_counts.pickle")
 if not os.path.exists(NAME_COUNTS_PATH):
     NAME_COUNTS_PATH = "https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/s2and-release/name_counts.pickle"
-    
+
 FASTTEXT_PATH = os.path.join(CONFIG["main_data_dir"], "lid.176.bin")
 if not os.path.exists(FASTTEXT_PATH):
     FASTTEXT_PATH = "https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/s2and-release/lid.176.bin"

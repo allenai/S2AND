e1246088ded7059dcaf12576620b8c36d00cfc63
Sergey Feldman <sergey@s2-elanding-24.reviz.ai2.in>
2022-08-02 21:47:40 +0000

    cluster tests pass

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,36 +1,32 @@
 import unittest
 import pytest
 import numpy as np
-import pickle
 
-from s2and.data import ANDData
+from s2and.data import PDData
 from s2and.model import Clusterer
-from s2and.featurizer import FeaturizationInfo, many_pairs_featurize
-from s2and.consts import LARGE_DISTANCE
+from s2and.featurizer import FeaturizationInfo
 import lightgbm as lgb
 
 
 class TestClusterer(unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.dummy_dataset = ANDData(
-            "tests/dummy/signatures.json",
-            "tests/dummy/papers.json",
-            clusters="tests/dummy/clusters.json",
-            cluster_seeds="tests/dummy/cluster_seeds.json",
-            name="dummy",
-            load_name_counts=True,
+        self.dataset = PDData(
+            "tests/test_dataset/papers.json",
+            clusters="tests/test_dataset/clusters.json",
+            name="test_dataset",
+            load_name_counts=True
         )
 
         features_to_use = [
             "year_diff",
-            "misc_features",
+            "title_similarity",
         ]
         featurizer_info = FeaturizationInfo(features_to_use=features_to_use)
         np.random.seed(1)
-        X_random = np.random.random((10, 6))
-        y_random = np.random.randint(0, 6, 10)
-        self.dummy_clusterer = Clusterer(
+        X_random = np.random.random((10, 3))
+        y_random = np.random.randint(0, 3, 10)
+        self.clusterer = Clusterer(
             featurizer_info=featurizer_info,
             classifier=lgb.LGBMClassifier(random_state=1, data_random_seed=1, feature_fraction_seed=1).fit(
                 X_random, y_random
@@ -40,41 +36,33 @@ class TestClusterer(unittest.TestCase):
             use_default_constraints_as_supervision=False,
         )
 
-    def test_get_constraints(self):
-        block = {
-            "a sattar": ["0", "1", "2"],
-        }
-        constraint_1 = self.dummy_dataset.get_constraint("0", "1", low_value=0, high_value=2)
-        constraint_2 = self.dummy_dataset.get_constraint("1", "0", low_value=0, high_value=2)
-        constraint_3 = self.dummy_dataset.get_constraint("1", "2", low_value=0, high_value=2)
-        constraint_4 = self.dummy_dataset.get_constraint("2", "1", low_value=0, high_value=2)
-
-        self.assertIs(constraint_1, LARGE_DISTANCE)
-        self.assertIs(constraint_2, LARGE_DISTANCE)
-        self.assertIs(constraint_3, 0)
-        self.assertIs(constraint_4, 0)
-
     def test_make_distance_matrix_fastcluster(self):
         block = {
-            "a sattar": ["0", "1", "2"],
+            'reviewerlistfor': [
+                '84177344',
+                '49188235',
+                '214237506',
+                '217917498', 
+                '1473469382'
+            ]
         }
-        partial_supervision = {("0", "1"): 1.1, ("1", "2"): 1e-6}
-        distance_matrices = self.dummy_clusterer.make_distance_matrices(
+        partial_supervision = {("84177344", "49188235"): 1.1, ("49188235", "214237506"): 1e-6}
+        distance_matrices = self.clusterer.make_distance_matrices(
             block_dict=block,
-            dataset=self.dummy_dataset,
+            dataset=self.dataset,
             partial_supervision=partial_supervision,
         )
-        distance_matrix = distance_matrices["a sattar"]
+        distance_matrix = distance_matrices["reviewerlistfor"]
         self.assertEqual(distance_matrix[0], np.float16(1.1))
         self.assertEqual(distance_matrix[1], np.float16(0.3))
         self.assertEqual(distance_matrix[2], np.float16(1e-6))
 
-        distance_matrices = self.dummy_clusterer.make_distance_matrices(
+        distance_matrices = self.clusterer.make_distance_matrices(
             block_dict=block,
-            dataset=self.dummy_dataset,
+            dataset=self.dataset,
             partial_supervision={},
         )
-        distance_matrix = distance_matrices["a sattar"]
+        distance_matrix = distance_matrices["reviewerlistfor"]
         self.assertEqual(distance_matrix[0], np.float16(0.3))
         self.assertEqual(distance_matrix[1], np.float16(0.3))
         self.assertEqual(distance_matrix[2], np.float16(0.3))

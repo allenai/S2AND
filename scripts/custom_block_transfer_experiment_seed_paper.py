26d6291662449418de0a17d2615f872a4c1d87e3
Aman Singh <amanpreets@s2-elanding-23.reviz.ai2.in>
2023-01-30 13:11:30 -0800

    black formatting issue fixed

--- scripts/custom_block_transfer_experiment_seed_paper.py
+++ scripts/custom_block_transfer_experiment_seed_paper.py
@@ -382,7 +382,7 @@ def main(
     gender_ethnicity_available: bool,
     use_cache: bool,
     custom_block_path: str,
-    embedding_suffix: str = "_specter.pickle"
+    embedding_suffix: str = "_specter.pickle",
 ):
     if not os.path.exists(os.path.join(DATA_DIR, "experiments", experiment_name, f"seed_{random_seed}", "metrics")):
         os.makedirs(
@@ -1407,7 +1407,7 @@ if __name__ == "__main__":
             args.gender_ethnicity_available,
             args.use_cache,
             args.custom_block_path,
-            args.emb_suffix
+            args.emb_suffix,
         )
         multi_b3_grid.append(b3_f1_grid)
         multi_pairwise_auroc_grid.append(pairwise_auroc_grid)
